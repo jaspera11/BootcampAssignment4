8a03ebbc07dc68ecac1cbbd0de14cc2168700f57
Alexander Jasper <jaspera11@ufl.com>
2018-09-25 23:45:18 -0400

    Assignment 4 Push2

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,9 @@
 module.exports = {
     db: {
         uri: 'mongodb://JasperA11:Panthers2016@ds155252.mlab.com:55252/uf-directory-app-assignment', //place the URI of your mongo database here.
-    }
+    },
+    port : 8080
 };
+
 
 /* Now go to the JSONtoMongo.js file and include this file as a variable named 'config' with a require() *///This file holds any configuration variables we may need 
